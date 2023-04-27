850032ab17449d24a034a39a438ca0561f960cc4
msetrainings <mse_training@proton.me>
2023-04-27 20:53:43 +0000

    add modules

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const request = require("request")
 const app = express()
 const port = 3000
-const date = require(__dirname + "/date.js")
+const date = require(__dirname + "/modules/date.js")
 
 app.use(express.static("public"));
 app.use(express.json())
